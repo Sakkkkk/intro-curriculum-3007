d779996a9b0bc61be52898bc8c7cba562fa797e3
Yos <yoshida.arw.ak@gmail.com>
2021-03-08 21:46:54 +0900

    17の倍数

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 'use strict';
+/**
+ *
+ * @param {number} 数値
+ */
+
+function isMultipleOfSeventeen(number) {
+    return number % 17 === 0;
+}
 
 module.exports = {
-}
+    isMultipleOfSeventeen
+};
